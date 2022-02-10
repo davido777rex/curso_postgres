5c50848ac37cc31b2e06e3436dfb8f780a089134
davido777rex <davzer@hotmail.com>
2022-02-10 03:00:52 -0500

    modificando la url del config

--- config/config.js
+++ config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'dev',
+  isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   dbUser:  process.env.DB_USER || 'nico',
   dbPassword:  process.env.DB_PASSWORD || 'admin123',
   dbHost:  process.env.DB_HOST || 'localhost',
   dbName:  process.env.DB_NAME || 'my_Store',
   dbPort:  process.env.DB_PORT || '5432',
+  dbUrl:  process.env.DATABASE_URL,
 }
 
 module.exports = { config };
--- db/config.js
+++ db/config.js
@@ -4,14 +4,16 @@ const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 // const URI = `postgres://root:admin123@localhost:3306/my_store`;
-console.log("holiwi")
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
   },
   production: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
