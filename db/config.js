f079b13c52c39b5704b843183c52b9311e7a9ae6
davido777rex <davzer@hotmail.com>
2022-02-10 02:45:29 -0500

    curso con backend funcionando v2

---
+++ db/config.js
@@ -0,0 +1,17 @@
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `postgres://root:admin123@localhost:3306/my_store`;
+console.log("holiwi")
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+}
