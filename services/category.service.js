7b3b82fb3d5128a8f20d5153b73ac10b2df1886b
davido777rex <davzer@hotmail.com>
2022-02-10 02:39:33 -0500

    curso con backend funcionando

--- services/category.service.js
+++ services/category.service.js
@@ -1,19 +1,25 @@
 const boom = require('@hapi/boom');
+const { models }= require('../lib/sequelize');
 
 class CategoryService {
 
   constructor(){
   }
   async create(data) {
-    return data;
+    const newCategory = await models.Category.create(data);
+    return newCategory;
   }
 
   async find() {
-    return [];
+    const categories = await models.Category.findAll();
+    return categories;
   }
 
   async findOne(id) {
-    return { id };
+    const category = await models.Category.findByPk(id, {
+      include: ['products']
+    });
+    return category;
   }
 
   async update(id, changes) {
--- services/product.service.js
+++ services/product.service.js
@@ -1,6 +1,11 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+const { Op } = require('sequelize');
+
+
+const { models } = require("../lib/sequelize")
+
 class ProductsService {
 
   constructor(){
@@ -22,16 +27,36 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find(query) {
+    const options = {
+      include: ['category'],
+      where: {}
+    }
+    const { limit, offset } = query;
+    if (limit && offset) {
+      options.limit = limit;
+      options.offset = offset;
+    }
+
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+
+    const { price_min, price_max } = query;
+    if (price_min && price_max){
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      }
+    }
+
+    const products = await models.Product.findAll(options)
+    return products;
   }
 
   async findOne(id) {
